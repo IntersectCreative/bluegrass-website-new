7c94ca9773240da2a458c0c01e480c11deb5f4fd
maymeclayton <mayme.clayton@gmail.com>
2019-08-29 12:06:31 -0400

    fixes navigation and double scrollbar issue

--- web/main.js
+++ web/main.js
@@ -3,11 +3,14 @@ $(window).scroll(function () {
   var scrollTop = $(window).scrollTop();
 
   if (scrollTop > 200) {
-    $('.header').css('display', 'block');
+    $('.header').css('display', 'flex');
     $('.header').addClass('fixed-to-top');
+    $('.header').addClass('active');
 
   } else if (scrollTop == 0) {
 
     $('.header').removeClass('fixed-to-top');
+    $('.header').removeClass('active');
+
   }
 });
